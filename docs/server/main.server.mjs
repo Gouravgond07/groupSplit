2807bef2c1c70a45027389a92384f9d41980e116
gourav <gouravgond07@gmail.com>
2024-06-02 21:52:09 +0530

    deploy

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-EG6X3C5F.mjs";import"./chunk-SDKBUKIA.mjs";import"./chunk-GFEKWSU2.mjs";import"./chunk-F7GM2IIK.mjs";import"./chunk-AERO2GNV.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
